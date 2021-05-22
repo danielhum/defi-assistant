8fcd16e763fb31cd2e4fcfe3df4563b70d61e303
Daniel Hum <dan@oneuphero.com>
2021-05-22 11:19:42 +0800

    optimize to call price api once per unique pair

--- app/workers/price_alert_worker.rb
+++ app/workers/price_alert_worker.rb
@@ -6,8 +6,15 @@ class PriceAlertWorker
   PUSHBULLET_ACCOUNT_EMAIL=ENV['PUSHBULLET_ACCOUNT_EMAIL'].freeze
   
   def perform(*args)
+    pairs = PriceAlert.select('DISTINCT ON (coin) coin, currency')
+                      .map{ |p| [p.coin, p.currency] }
+    prices = pairs.inject({}) do |h, pair|
+      h[pair] = PriceAlert.new(coin: pair[0], currency: pair[1]).get_current_price
+      h
+    end
+
     PriceAlert.find_each do |price_alert|
-      price = price_alert.get_current_price
+      price = prices[[price_alert.coin, price_alert.currency]]
       alert = price_alert.alert(price)
       PushBullet.create_push(email: PUSHBULLET_ACCOUNT_EMAIL, body: alert) if alert
       price_alert.update_last_price!(price)
