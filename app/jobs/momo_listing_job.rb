e0c76244889d952b2b32b97f7828901231280e4b
Daniel Hum <dan@oneuphero.com>
2021-08-14 20:06:29 +0800

    schedule jobs

--- app/jobs/momo_listing_job.rb
+++ app/jobs/momo_listing_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_dependency "push_bullet"
 require_dependency "momo_market_api"
 
@@ -6,11 +8,12 @@ class MomoListingJob < ApplicationJob
 
   LOW_PRICE_THRESHOLD = 750
   NUM_PAGES = 5
+  LAST_PUSH_KEY = "momo_listing_job:last_push".freeze
+  PUSH_COOLDOWN = 3600 # in seconds
 
   def perform(*args)
     attrs = MomoListing.attribute_names.dup
     attrs.delete("id")
-    now = Time.now
 
     list = []
     NUM_PAGES.times do |i|
@@ -18,6 +21,8 @@ class MomoListingJob < ApplicationJob
       list += res["list"]
       sleep rand(1..5)
     end
+
+    now = Time.now
     insert_data = list.map do |listing|
       listing.select! { |k, _| attrs.include?(k.underscore) }
       data = listing.each_with_object({}) { |(k, v), h| h[k.underscore] = v }
@@ -32,7 +37,9 @@ class MomoListingJob < ApplicationJob
       .insert_all(insert_data, returning: %w[now_price]).rows.flatten
 
     low_price = (prices.min)/(10**9)
-    if low_price <= LOW_PRICE_THRESHOLD
+    last_push = Integer(REDIS.get(LAST_PUSH_KEY) || 0)
+    if low_price <= LOW_PRICE_THRESHOLD && (now.to_i - last_push > PUSH_COOLDOWN)
+      REDIS.set LAST_PUSH_KEY, now.to_i
       PushBullet.create_push(
         body: "Momo Listing <= #{LOW_PRICE_THRESHOLD}: $#{low_price}"
       )
