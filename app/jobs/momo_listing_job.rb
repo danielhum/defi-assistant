2d315beaaa0aaa1ea0938a4bedaee9ab0728e48b
Daniel Hum <dan@oneuphero.com>
2021-09-16 18:30:41 +0800

    update momo listing detection

--- app/jobs/momo_listing_job.rb
+++ app/jobs/momo_listing_job.rb
@@ -6,10 +6,12 @@ require_dependency "momo_market_api"
 class MomoListingJob < ApplicationJob
   queue_as :default
 
-  LOW_PRICE_THRESHOLD = ENV['MOMO_LOW_PRICE'].to_i
+  LOW_PRICE_THRESHOLD = (ENV['MOMO_LOW_PRICE'] || 3000).to_i
+  VERY_LOW_PRICE_THRESHOLD = (ENV['MOMO_VERY_LOW_PRICE'] || 1000).to_i
   MAX_PAGES = 10
   LAST_PUSH_KEY = "momo_listing_job:last_push".freeze
   PUSH_COOLDOWN = 3600 # in seconds
+  URGENT_PUSH_COOLDOWN = 180 # in seconds
 
   def perform(max_pages: nil)
     attrs = MomoListing.attribute_names.dup
@@ -40,18 +42,29 @@ class MomoListingJob < ApplicationJob
         "updated_at" => now
       })
     end
-    prices = MomoListing
-      .insert_all(insert_data, returning: %w[now_price]).rows.flatten
+    listings = MomoListing
+      .insert_all(insert_data, returning: %w[now_price hashrate]).rows
+    return if listings.empty?
 
-    if prices.count > 0
-      low_price = (prices.min)/(10**9)
-      last_push = Integer(REDIS.get(LAST_PUSH_KEY) || 0)
-      if low_price <= LOW_PRICE_THRESHOLD && (now.to_i - last_push > PUSH_COOLDOWN)
-        REDIS.set LAST_PUSH_KEY, now.to_i
-        PushBullet.create_push(
-          body: "Momo Listing <= #{LOW_PRICE_THRESHOLD}: $#{low_price}"
-        )
-      end
+    cheap_price =
+      listings.map { |l| price_to_dollars(l[0]) }.select do |p|
+        low_price?(p, VERY_LOW_PRICE_THRESHOLD)
+      end.min
+
+    if cheap_price && should_push?(URGENT_PUSH_COOLDOWN)
+      push("Very cheap Momo: $#{cheap_price}")
+    end
+
+    good_listings = listings.select { |l| l[1] > 30 } # hashrate > 30
+    prices, hashrates =
+      good_listings.each_with_object([[], []]) do |(price, hashrate), a|
+      a[0] << price_to_dollars(price)
+      a[1] << hashrate
+    end
+    low_price = prices.min
+
+    if low_price?(low_price) && should_push?(PUSH_COOLDOWN)
+      push("Momo Listing <= #{LOW_PRICE_THRESHOLD}: $#{low_price}")
     end
   end
 
@@ -59,5 +72,25 @@ class MomoListingJob < ApplicationJob
 
   def id_exists?(raw_ids:)
     MomoListing.where(raw_id: raw_ids).exists?
+  end
+
+  def price_to_dollars(price)
+    price ? price/(10**9) : nil
+  end
+
+  def low_price?(price, threshold = LOW_PRICE_THRESHOLD)
+    price <= threshold
+  end
+
+  def should_push?(cooldown)
+    last_push = Integer(REDIS.get(LAST_PUSH_KEY) || 0)
+    Time.now.to_i - last_push > cooldown
+  end
+
+  def push(body)
+    Rails.logger.info "pushing! #{body}"
+    now = Time.now.to_i
+    REDIS.set LAST_PUSH_KEY, now
+    PushBullet.create_push body: body  
   end
 end
